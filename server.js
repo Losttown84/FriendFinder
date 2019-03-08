bdbe5d3d10a58a8c45f0b679b82a40e461604fe4
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-03-08 14:41:18 -0600

    added in pseudo code for body parsing

--- server.js
+++ server.js
@@ -1,2 +1,5 @@
+// I looked at others people's githubs and saw this body parser var and the use of it for json and text. When this is graded, can I get some more light as to what this does for the files in lamens terms?
+
 var express = require('express');
+var bodyParser = require('body-parser');
 var path = require('path');
