7cf6600f482b2ee48a73a237416d2f32e1b70ece
Mike Plass <43932200+Losttown84@users.noreply.github.com>
2019-03-08 15:41:27 -0600

    public and routing folders weren't aligned in app.

--- server.js
+++ server.js
@@ -1,5 +1,27 @@
 // I looked at others people's githubs and saw this body parser var and the use of it for json and text. When this is graded, can I get some more light as to what this does for the files in lamens terms?
 
+
 var express = require('express');
 var bodyParser = require('body-parser');
 var path = require('path');
+
+//  Express application
+var app = express();
+var PORT = process.env.PORT;
+
+// Uncovering the directory to access CSS files
+app.use(express.static(path.join(__dirname, './app/public')));
+
+// Add middleware for parsing incoming request bodies
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.text());
+
+// Add the application routes
+require(path.join(__dirname, './app/routing/apiRoutes'))(app);
+require(path.join(__dirname, './app/routing/htmlRoutes'))(app);
+
+// Start listening on PORT
+app.listen(PORT, function() {
+  console.log('Friend Finder app is listening on PORT: ' + PORT);
+});
